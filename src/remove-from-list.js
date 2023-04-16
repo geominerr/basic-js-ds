96f39c25b04cadc9371f749e5bb7ebc448cdc8b4
Maksym Voloshyn <mv.webdew@gmail.com>
2023-04-16 16:00:24 +0300

    feat: add a solution to the remove-from-list task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -11,8 +11,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * @return {List}
  *
  * @example
- * For l = [3, 1, 2, 3, 4, 5] and k = 3,
- * the output should be [1, 2, 4, 5]
+ * For l = 3, 1, 2, 3, 4, 5 and k = 3,
+ * the output should be 1, 2, 4, 5
  *
  * Singly - linked lists are already defined using interface
  * class ListNode {
@@ -22,11 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(list, k) {
+  if (list.value === k) {
+    list = list.next;
+  }
+
+  let current = list;
+
+  while (current.next) {
+    if (current.next.value === k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return list;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
