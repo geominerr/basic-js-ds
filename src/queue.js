1eefc877fc0af8674cee601d764b1b13e35130c7
Maksym Voloshyn <mv.webdew@gmail.com>
2023-04-16 22:15:55 +0300

    feat: add a solution to the queue task without getUnderlyingList()

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js");
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,23 +14,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    let current = this.head;
+    let next = this.head.next;
+    const list = {};
+    list.value = current;
+    list.next = next;
+    this.length -= 1;
+
+    while (this.length > 0) {
+      current = current.next;
+      list.next = current;
+      this.length -= 1;
+    }
+
+    return list;
+  }
+
+  enqueue(value) {
+    const node = new ListNode(value);
+
+    if (this.head) {
+      this.tail.next = node;
+      this.tail = node;
+    } else {
+      this.head = node;
+      this.tail = node;
+    }
+
+    this.length += 1;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const current = this.head;
+    this.head = this.head.next;
+    this.length -= 1;
+
+    return current.value;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
+
+const que = new Queue();
+que.enqueue(2);
+que.enqueue(4);
+que.enqueue("bla");
+console.log(que.getUnderlyingList());
