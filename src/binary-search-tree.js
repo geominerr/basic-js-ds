d22ddf47b21c9a123c28b22af9eead63af816f77
Maksym Voloshyn <mv.webdew@gmail>
2023-04-17 20:00:45 +0300

    feat: add a method remove() BST

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -78,9 +78,52 @@ class BinarySearchTree {
     }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.rootTree = removeNode(this.rootTree, data);
+
+    function removeNode(node, data) {
+      if (!node) {
+        return null;
+      }
+
+      if (data < node.data) {
+        node.left = removeNode(node.left, data);
+
+        return node;
+      } else if (data > node.data) {
+        node.right = removeNode(node.right, data);
+
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          return null;
+        }
+
+        if (!node.left) {
+          node = node.right;
+
+          return node;
+        }
+
+        if (!node.right) {
+          node = node.left;
+
+          return node;
+        }
+
+        let maxLeft = node.left;
+
+        while (maxLeft.right) {
+          maxLeft = maxLeft.right;
+        }
+
+        node.data = maxLeft.data;
+        node.left = removeNode(node.left, maxLeft.data)
+
+        return node;
+      }
+
+    }
   }
 
   min() {
@@ -109,5 +152,6 @@ bst.add(13);
 bst.add(14);
 
 
-console.log(bst.rootTree);
-console.log(bst.find(26))
+console.log(bst.find(26));
+bst.remove(26);
+console.log(bst.root())
