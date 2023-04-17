563f08a17a1fe7d4ad7072c8038a25a0cea1aa53
Maksym Voloshyn <mv.webdew@gmail>
2023-04-17 18:26:37 +0300

    feat: add a method add() BST

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,21 +1,41 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
 
   root() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.root = addData(this.root, data);
+
+    function addData(node, data) {
+      if (!node) {
+        return new Node(data)
+      }
+
+      if (node.data === data) {
+        return node;
+      }
+
+      if (data < node.data) {
+        node.left = addData(node.left, data);
+      } else {
+        node.left = addData(node.left, data);
+      }
+
+      return node;
+    }
   }
 
   has(/* data */) {
